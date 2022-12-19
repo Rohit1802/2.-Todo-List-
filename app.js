03fcfb0a77e7cf4490e91927bc6f6cb6466b2b09
Rohit Kalubarme <67742127+Rohit1802@users.noreply.github.com>
2022-12-19 22:22:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//Selectors
+//Selectors used are querySelector
 const todoInput = document.querySelector('.todo-input')
 const todoButton = document.querySelector('.todo-button')
 const todoList = document.querySelector('.todo-list')
